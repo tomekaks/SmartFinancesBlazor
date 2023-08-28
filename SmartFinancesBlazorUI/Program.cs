732b3222ea80045064bf0930a76816020cd86011
tomekaks <tomekaks@gmail.com>
2023-08-28 21:51:26 +0200

    Small changes

--- SmartFinancesBlazorUI/Program.cs
+++ SmartFinancesBlazorUI/Program.cs
@@ -12,6 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ITransfersService, TransfersService>();
+builder.Services.AddScoped<IContactsService, ContactsService>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 await builder.Build().RunAsync();
