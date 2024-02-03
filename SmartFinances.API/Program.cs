2313f19cd7b121e9cbd8021e7b21033f525977a2
tomekaks <tomekaks@gmail.com>
2024-02-03 16:48:36 +0100

    SwaggerUI outside of the developement environment

--- SmartFinances.API/Program.cs
+++ SmartFinances.API/Program.cs
@@ -29,9 +29,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
--- SmartFinancesBlazorUI/Models/BudgetPlanner/PlannerVM.cs
+++ SmartFinancesBlazorUI/Models/BudgetPlanner/PlannerVM.cs
@@ -8,16 +8,6 @@
         public decimal Budget { get; set; }
         public decimal TotalAmount { get => GetTotalAmount(); }
         public decimal Saved { get => Budget - TotalAmount; }
-        public decimal HousingAmount { get => GetTotalAmountByExpenseType(Constants.HOUSING); }
-        public decimal UtilitiesAmount { get => GetTotalAmountByExpenseType(Constants.UTILITIES); }
-        public decimal FoodAmount { get => GetTotalAmountByExpenseType(Constants.FOOD); }
-        public decimal ClothesAmount { get => GetTotalAmountByExpenseType(Constants.CLOTHES); }
-        public decimal HealthAmount { get => GetTotalAmountByExpenseType(Constants.HEALTH); }
-        public decimal EntertainmentAmount { get => GetTotalAmountByExpenseType(Constants.ENTERTAINMENT); }
-        public decimal ElectronicsAmount { get => GetTotalAmountByExpenseType(Constants.ELECTRONICS); }
-        public decimal HouseholdAmount { get => GetTotalAmountByExpenseType(Constants.HOUSEHOLD); }
-        public decimal TransportationAmount { get => GetTotalAmountByExpenseType(Constants.TRANSPORTATION); }
-        public decimal PersonalAmount { get => GetTotalAmountByExpenseType(Constants.PERSONAL); }
 
         private decimal GetTotalAmount()
         {
@@ -31,22 +21,6 @@
                 }
             }
             return totalAmount;
-        }
-
-        private decimal GetTotalAmountByExpenseType(string expenseType)
-        {
-            decimal amount = 0;
-            var expenses = CurrentMonthlySummary.Expenses.FindAll(q => q.ExpenseTypeVM.Name == expenseType);
-
-            if(expenses != null && expenses.Any())
-            {
-                foreach (var item in expenses)
-                {
-                    amount += item.Amount;
-                }
-            }
-
-            return amount;
         }
     }
 }
